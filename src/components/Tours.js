1fdadf84e7a8fd186e6f21a8f079477628ca7552
Nurlan <nurlanimamali@gmail.com>
2023-11-14 16:44:36 +0100

    styling

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,22 +1,25 @@
 import React from "react";
 import "./Tours.css";
+import { Container } from "react-bootstrap";
 
 const Tours = ({ tour }) => {
   const { description, images } = tour;
   return (
-    <div className="tours">
-      <div className="tour-container">
-        <div className="tour-text">
-          <h3>{description.heading}</h3>
-          <p>{description.text}</p>
-        </div>
-        <div className="image-container">
-          {images.map((image, index) => (
-            <img key={index} alt={`tour${index}`} src={image} />
-          ))}
+    <Container>
+      <div className="tours">
+        <div className="tour-container">
+          <div className="tour-text">
+            <h3>{description.heading}</h3>
+            <p>{description.text}</p>
+          </div>
+          <div className="image-container">
+            {images.map((image, index) => (
+              <img key={index} alt={`tour${index}`} src={image} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
