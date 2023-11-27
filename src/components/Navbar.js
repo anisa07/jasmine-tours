10abe9ec3a676a96da2e9e160bd1253389171b95
Nurlan <nurlanimamali@gmail.com>
2023-11-27 19:11:57 +0100

    logo animation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,16 +4,17 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import "./Navbar.css";
+import LogoSvg from "./LogoAnimation";
 
 function Navigation() {
   return (
-    <Navbar expand="lg" className="navbar-custom-bg">
+    <Navbar expand="lg" className="navbar-custom-bg sticky-top">
       <Container fluid>
-        <Navbar.Brand href="/" className="me-3">
-          Jasmine Tours
+        <Navbar.Brand href="/" className="custom-brand">
+          <LogoSvg />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
           <Nav className="ms-auto" style={{ lineHeight: "2em" }}>
             <Nav.Link
               href="/excursions"
@@ -31,18 +32,18 @@ function Navigation() {
               className="custom-nav-link"
               title="Связаться"
               id="basic-nav-dropdown"
-              menuVariant="dark"
+              menuVariant="white"
             >
               <NavDropdown.Item
                 href="https://wa.me/c/31642221525"
-                className="text-white"
+                className="custom-dropdown-icons"
               >
                 <i className="fa-brands fa-whatsapp fa-beat icon-margin m-2" />
                 ВП
               </NavDropdown.Item>
               <NavDropdown.Item
                 href="https://www.instagram.com/jasmine.tours.amsterdam/"
-                className="text-white"
+                className="custom-dropdown-icons"
               >
                 <i className="fa-brands fa-instagram fa-beat icon-margin m-2" />
                 ИН
