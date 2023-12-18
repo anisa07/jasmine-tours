04a44059d2037f23f8b60d83d315dea89f36bac6
Nurlan <nurlanimamali@gmail.com>
2023-12-18 14:47:41 +0100

    button for hangburger menu fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,11 +11,14 @@ function Navigation() {
     <Navbar expand="lg" className="navbar-custom-bg sticky-top">
       <Container fluid>
         <Navbar.Brand href="/" className="custom-brand">
-          <LogoSvg className="logo-animation"/>
+          <LogoSvg className="logo-animation" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <Nav className="ms-auto" style={{ lineHeight: "2em" }}>
+          <Nav
+            className="ml-auto align-items-center"
+            style={{ lineHeight: "2em" }}
+          >
             <Nav.Link
               href="/excursions"
               className="text-decoration-none px-3 py-2 hover-effect custom-nav-link"
