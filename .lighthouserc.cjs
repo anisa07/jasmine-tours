a292ca519625fd057dd4e82c8c032f2106b10141
Roman Iovlev <romanyister@gmail.com>
2025-10-13 12:47:26 +0200

    setup lighthouse, add robot and site map, fix seo and accessibility issues

---
+++ .lighthouserc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: [
+        'http://localhost:4321/',
+        'http://localhost:4321/ru/about-me/',
+      ],
+      numberOfRuns: 3,
+      settings: {
+        preset: 'desktop',
+        throttling: {
+          rttMs: 40,
+          throughputKbps: 10240,
+          cpuSlowdownMultiplier: 1,
+        },
+        screenEmulation: {
+          mobile: false,
+          width: 1350,
+          height: 940,
+          deviceScaleFactor: 1,
+          disabled: false,
+        },
+        formFactor: 'desktop',
+        onlyCategories: ['performance', 'accessibility', 'best-practices', 'seo'],
+        skipAudits: [],
+      },
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.95 }],
+        'categories:accessibility': ['error', { minScore: 0.95 }],
+        'categories:best-practices': ['error', { minScore: 0.95 }],
+        'categories:seo': ['error', { minScore: 0.95 }],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
