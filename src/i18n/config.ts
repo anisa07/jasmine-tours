352901ff6adebe4ac7eae71dc231317bf404e57c
anisa07 <askany07@gmail.com>
2025-10-17 09:40:08 +0200

    remove locale

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,8 +1,9 @@
-export type Locale = "ru" | "en";
+export type Locale = "ru" | "en" | "nl";
 export const locales: Locale[] = ["ru"];
 export const defaultLocale: Locale = "ru";
 
 export const localeNames: Record<Locale, string> = {
   ru: "Русский",
   en: "English",
+  nl: "Nederlands",
 };
